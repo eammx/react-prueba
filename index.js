18c64d215b4e1d730c5704bf9ee55ed135a70ee7
eammx <eamm2009@gmail.com>
2020-04-11 13:50:48 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import Hello from './Hello'; 
 import './style.css';
 
 const SaludarEnIdiomas = ({idioma = 'en',nombre = 'nombre'}) =>{
   const saludo = idioma === 'es'?'Hola':'Hello';
   return <p>{saludo} {nombre}</p>
+  }
+
+  const Button =() =>{
+    const [conteo,setConteo]= useState(0);
+    return (
+      <div>
+      <p>Presionado: {conteo}</p>
+      <button onClick = { ()=>setConteo(conteo +1)}>Click en mi!</button>
+      </div>
+    )
   }
 
 class App extends Component {
@@ -25,7 +35,8 @@ class App extends Component {
         <p>
           Iniciar aplicación :-)
         </p>
-      </div>
+        <Button/>
+      </div>      
     );
   }
 }
